66c940a2766cd4af791279b5ff22a5058491e662
roye yu <royeyu@royedeMacBook-Pro.local>
2017-07-19 12:52:10 +0800

    去除chromeinput 输入黄色背景

---
+++ src/style/LoginPage.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  'login-content:after': {
+    'content': ''.'',
+    'display': 'block',
+    'height': [{ 'unit': 'px', 'value': 0 }],
+    'visibility': 'hidden',
+    'clear': 'both'
+  },
+  'input:-webkit-autofill': {
+    'boxShadow': [{ 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 1000 }, { 'unit': 'string', 'value': 'white' }, { 'unit': 'string', 'value': 'inset' }]
+  }
+});
